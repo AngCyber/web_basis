c15a911ed67b5c04caf60e0ca0640b9526063826
Moch Aang XD <92802033+AngCyber@users.noreply.github.com>
2022-07-28 04:47:07 +0700

    Create snap.js

---
+++ funding/snap.js
@@ -0,0 +1,173 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`usePosition should return empty values by default 1`] = `
+Array [
+  <div>
+    Trying to fetch location...
+  </div>,
+  <br />,
+  <code>
+    latitude: 
+    <br />
+    longitude: 
+    <br />
+    timestamp: 
+    <br />
+    accuracy: 
+    <br />
+    speed: 
+    <br />
+    heading: 
+    <br />
+    error: 
+  </code>,
+]
+`;
+
+exports[`usePosition should return empty values by default in watching mode 1`] = `
+<code>
+  latitude: 
+  <br />
+  longitude: 
+  <br />
+  timestamp: 
+  <br />
+  accuracy: 
+  <br />
+  speed: 
+  <br />
+  heading: 
+  <br />
+  error: 
+  Geolocation is not supported
+</code>
+`;
+
+exports[`usePosition should return error if navigator is not supported 1`] = `
+<code>
+  latitude: 
+  <br />
+  longitude: 
+  <br />
+  timestamp: 
+  <br />
+  accuracy: 
+  <br />
+  speed: 
+  <br />
+  heading: 
+  <br />
+  error: 
+  Geolocation is not supported
+</code>
+`;
+
+exports[`usePosition should return error while fetching 1`] = `
+<code>
+  latitude: 
+  <br />
+  longitude: 
+  <br />
+  timestamp: 
+  <br />
+  accuracy: 
+  <br />
+  speed: 
+  <br />
+  heading: 
+  <br />
+  error: 
+  User denied Geolocation
+</code>
+`;
+
+exports[`usePosition should return error while watching 1`] = `
+<code>
+  latitude: 
+  <br />
+  longitude: 
+  <br />
+  timestamp: 
+  <br />
+  accuracy: 
+  <br />
+  speed: 
+  <br />
+  heading: 
+  <br />
+  error: 
+  User denied Geolocation
+</code>
+`;
+
+exports[`usePosition should return latitude and longitude even if watch is undefined 1`] = `
+<code>
+  latitude: 
+  52.3172414
+  <br />
+  longitude: 
+  4.8717809
+  <br />
+  timestamp: 
+  1561815013194
+  <br />
+  accuracy: 
+  24 meters
+  <br />
+  speed: 
+  0
+  <br />
+  heading: 
+  90 degrees
+  <br />
+  error: 
+</code>
+`;
+
+exports[`usePosition should return latitude and longitude while fetching 1`] = `
+<code>
+  latitude: 
+  52.3172414
+  <br />
+  longitude: 
+  4.8717809
+  <br />
+  timestamp: 
+  1561815013194
+  <br />
+  accuracy: 
+  24 meters
+  <br />
+  speed: 
+  0
+  <br />
+  heading: 
+  90 degrees
+  <br />
+  error: 
+</code>
+`;
+
+exports[`usePosition should return latitude and longitude while watching 1`] = `
+<code>
+  latitude: 
+  52.3172414
+  <br />
+  longitude: 
+  4.8717809
+  <br />
+  timestamp: 
+  1561815013194
+  <br />
+  accuracy: 
+  24 meters
+  <br />
+  speed: 
+  0
+  <br />
+  heading: 
+  90 degrees
+  <br />
+  error: 
+</code>
+`;
